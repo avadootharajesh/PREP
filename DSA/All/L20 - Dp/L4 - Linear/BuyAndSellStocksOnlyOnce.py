9f46e00a589c58afafdd1e76d0458d6d24d34c0e
avadootharajesh <avadootha.rajesh.netha@gmail.com>
2026-06-13 16:21:44 +0530

    Files added - Linear DP

---
+++ DSA/All/L20 - Dp/L4 - Linear/BuyAndSellStocksOnlyOnce.py
@@ -0,0 +1,13 @@
+# Maximize the profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+
+    return max_profit
